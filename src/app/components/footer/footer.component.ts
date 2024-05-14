260d243c7b213c70eed8aab1ca3c3be0b0b0a386
Brito <mateusvsbrito6@gmail.com>
2024-05-14 19:56:39 -0300

    feat: Footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  @Input()
+  endereco:string = ''
+  @Input()
+  contato:string = ''
+  @Input()
+  email:string = ''
+}
